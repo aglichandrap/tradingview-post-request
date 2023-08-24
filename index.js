fc14cb5a209c70ded021d4ce42fd96f571132b59
aglichandrap <aglichandrap@gmail.com>
2023-08-24 12:39:17 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser'); // To parse incoming JSON data
+
+const app = express();
+const PORT = process.env.PORT || 3333;
+
+// Middleware to parse JSON data
+app.use(bodyParser.json());
+
+// TradingView alert endpoint
+app.post('/tradingview/alert', (req, res) => {
+  const alertData = req.body; // Alert data sent from TradingView
+  
+  // Log the alert data (you can customize this part)
+  console.log('Received TradingView Alert:', alertData);
+  
+  // Send a response
+  res.status(200).json({ message: 'Alert received successfully' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
