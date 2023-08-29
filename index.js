26a6b7c35c449f4f505c9823d93fff92a13b9c30
aglichandrap <aglichandrap@gmail.com>
2023-08-29 10:58:23 +0700

    add alertdata

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const bodyParser = require('body-parser'); // To parse incoming JSON data
 
 const app = express();
 const PORT = process.env.PORT || 3333;
 
+// Middleware to parse JSON data
 app.use(bodyParser.json());
-
-// Allow requests from any origin
-app.use(cors());
-
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+// TradingView alert endpoint
 app.post('/tradingview/alert', (req, res) => {
-  const alertData = req.body;
+  const alertData = req.body; // Alert data sent from TradingView
+  
+  // Log the alert data (you can customize this part)
   console.log('Received TradingView Alert:', alertData);
-  res.status(200).json(alertData);
+  
+  // Send a response
+  res.status(200).json({ alertData });
 });
 
 app.listen(PORT, () => {
